948accaa113009036b8ce930dacd0d94ffbb9f67
Brendan Shields <brendan.m.j.shields@gmail.com>
2019-02-08 14:09:32 +1100

    fixing routing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,15 @@
-import React, { useState, Components } from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
-
 const [count, setCount] = useState(0)
 
 return (
     <div>
+
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
-      <nav> This is a NavBar </nav>
-      <h1> Welcome to the Opera House! </h1>
-      <h2> Slider </h2>
     </div>
   );
 }
