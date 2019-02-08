d63366a146e16837f89963e43cd3d619cbd6b82c
Brendan Shields <brendan.m.j.shields@gmail.com>
2019-02-08 11:22:44 +1100

    added npm router

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Opera House!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
