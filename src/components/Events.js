f3b95535c401966a9d3cd809ad2d41df968813bd
Elli <ellimoty@gmail.com>
2019-02-13 10:28:39 +1100

    switch component info deatils to events and worked on info component

--- src/components/Events.js
+++ src/components/Events.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import axios from 'axios';
 import "./css/categories.css";
+import './css/info.scss'
+import Dance from './images/dance.jpg';
 
 function Events(props) {
   // State
@@ -45,9 +47,33 @@ function Events(props) {
         }
       })
       }
-      <ul>
+      <ul className="info">
         {names.map((name) =>
-          <li className="grid-item"><Link to={'/events/' + props.match.params.genre + '/' + name.replace(/ /g, '_')}>{name}</Link></li>)}
+          <li>
+            <div className="blog-card">
+              <div className="meta">
+                <div className="photo" style={{ background: `url( ${Dance} )` }}></div>
+
+                <ul className="details">
+                  <li class="date">Aug 25 2019</li>
+                  <li class="tags">
+                    <ul>
+                      <li><Link to="/">Location</Link></li>
+                    </ul>
+                  </li>
+                </ul>
+
+              </div>
+              <div className="description">
+                <h1>{name}</h1>
+                <h2>Opening a door to the future</h2>
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>
+                <p className="read-more">
+                  <Link to={'/events/' + props.match.params.genre + '/' + name.replace(/ /g, '_')}>Read More</Link>
+                </p>
+              </div>
+            </div>
+          </li>)}
       </ul>
     </div>
   );
--- src/components/Info.js
+++ src/components/Info.js
@@ -26,31 +26,9 @@ function Info(props) {
     return (
         <div className="info">
             <h2>{name}</h2>
-            <div className="blog-card">
-                <div className="meta">
-                    <div className="photo" style={{ background: `url( ${Dance} )` }}></div>
-
-                    <ul className="details">
-                        <li class="date">Aug 25 2019</li>
-                        <li class="tags">
-                            <ul>
-                                <li><Link to="/">Location</Link></li>
-                            </ul>
-                        </li>
-
-
-                    </ul>
-
-                </div>
-                <div className="description">
-                    <h1>Learning to Code</h1>
-                    <h2>Opening a door to the future</h2>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>
-                    <p className="read-more">
-                        <Link to="/">Booking</Link>
-                    </p>
-                </div>
-            </div>
+            <img src={Dance} />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>
+            <button>Book Ticket</button>
         </div>
     );
 }
