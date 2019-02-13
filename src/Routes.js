40dc3989e786db68da4e89c5770ed5abd421bebd
Brendan Shields <brendan.m.j.shields@gmail.com>
2019-02-14 10:58:28 +1100

    seats svg function mapped out

--- src/Routes.js
+++ src/Routes.js
@@ -5,11 +5,13 @@ import Categories from './components/Categories';
 import Events from './components/Events';
 import Info from './components/Info';
 import Seats from './components/Seats';
+import Login from './components/Login'
 
 const Routes = () => (
     <Router class="navbar">
         <div>
-            <Route exact path="/" component={Home}/>    
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/login" component={Login}/>
             <Route exact path="/events" component={Categories}/>
             <Route exact path="/events/:genre" component={Events}/>
             <Route exact path="/events/:genre/:name" component={Info}/>
